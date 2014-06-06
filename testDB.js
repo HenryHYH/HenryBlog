8e557513005f31b10293412440e025c2039632c3
Henry <huyonghui@moneyking.cn>
2014-06-06 13:04:44 +0800

    Finish test db

--- testDB.js
+++ testDB.js
@@ -1,13 +1,24 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/HenryDB');
 
-var Cat = mongoose.model('Cat', { name: String });
+var User = mongoose.model('User', { Name: String }, 'User');
 
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
+if (false) {
+    var info = new User({ Name: 'ABC' });
+
+    info.save(function (err) {
+        if (err)
+            console.error(err);
+        console.log("saved");
+    });
+}
+
+User.find(function (err, list) {
     if (err)
-        console.log('meow');
-    console.log("saved");
+        console.error(err);
+    list.forEach(function (item) {
+        console.log(item);
+    });
 });
 
 console.log("Start");
