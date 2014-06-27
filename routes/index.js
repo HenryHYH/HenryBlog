256587fed09cdd6b78461a408d7c8bedc4d97fc2
Henry <henry.hyh@gmail.com>
2014-06-27 13:02:33 +0800

    Modify blog & gallery

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
 var express = require('express'),
-    router = express.Router(),
-    userDao = require('../dao/user');
+    router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    userDao.allUsers(function (err, users) {
-        if (err) {
-            console.error(err);
-        }
-        else {
-            console.log(users);
-        }
-    });
-
     res.render('index', {
         title: 'Express',
         len: 50,
